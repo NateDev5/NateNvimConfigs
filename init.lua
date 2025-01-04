e03a4f7a608564b7379980961fe116d9a4e575fe
NateDev5 <nathan_dev@outlook.com>
2025-01-03 20:09:55 -0500

    First commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+require("vim-options")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+
