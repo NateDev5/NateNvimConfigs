3aae5f09d242853e7d74f91c761585bffcb43db6
NateDev5 <nathan_bes@outlook.com>
2025-08-09 22:38:31 -0400

    fixed status column

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,4 +11,17 @@ vim.cmd.colorscheme("gruvbox")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.statuscolumn = "%s %{printf('%4d', v:lnum)} ┃ %{printf('%-4d', v:relnum)}"
+
+vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
+    callback = function()
+        vim.opt_local.statuscolumn = " %{printf('%3d', v:lnum)} ┃ %{printf('%-3d', v:relnum)}"
+    end
+})
+
+vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+        vim.opt_local.statuscolumn = " %{v:lnum}"
+    end
+})
+
+-- vim.opt.statuscolumn = "%s %{printf('%4d', v:lnum)} ┃ %{printf('%-4d', v:relnum)}"
